d4314ee42ce0f704f42ff7306b7ce232b7a1363b
Vincent Biret <vibiret@microsoft.com>
2020-09-16 08:26:24 -0400

    - applies code review suggestions

--- src/main/java/com/microsoft/graph/httpcore/ChaosHttpHandler.java
+++ src/main/java/com/microsoft/graph/httpcore/ChaosHttpHandler.java
@@ -13,17 +13,30 @@ import okhttp3.Request;
 import okhttp3.Response;
 import okhttp3.ResponseBody;
 
+/**
+ * DO NOT USE IN PRODUCTION
+ * interceptor that randomly fails the responses for unit testing purposes
+ */
 public class ChaosHttpHandler implements Interceptor {
-	
 	public final MiddlewareType MIDDLEWARE_TYPE = MiddlewareType.RETRY;
-
-    /*
-     * constant string being used
-     */
-    private final String RETRY_AFTER = "Retry-After";
-    
-    public static final int MSClientErrorCodeTooManyRequests = 429;
-    
+	/*
+	 * constant string being used
+	 */
+	private final String RETRY_AFTER = "Retry-After";
+	/**
+	 * Denominator for the failure rate (i.e. 1/X)
+	 */
+	private final Integer failureRate = 3;
+	/**
+	 * default value to return on retry after
+	 */
+	private final String retryAfterValue = "10";
+	/**
+	 * body to respond on failed requests
+	 */
+	private final String responseBody = "{\"error\": {\"code\": \"TooManyRequests\",\"innerError\": {\"code\": \"429\",\"date\": \"2020-08-18T12:51:51\",\"message\": \"Please retry after\",\"request-id\": \"94fb3b52-452a-4535-a601-69e0a90e3aa2\",\"status\": \"429\"},\"message\": \"Please retry again later.\"}}";
+	public static final int MSClientErrorCodeTooManyRequests = 429;
+	
 	@Override
 	public Response intercept(Chain chain) throws IOException {
 		Request request = chain.request();
@@ -31,22 +44,22 @@ public class ChaosHttpHandler implements Interceptor {
 		if(request.tag(TelemetryOptions.class) == null)
 			request = request.newBuilder().tag(TelemetryOptions.class, new TelemetryOptions()).build();
 		request.tag(TelemetryOptions.class).setFeatureUsage(TelemetryOptions.RETRY_HANDLER_ENABLED_FLAG);
-        
-        final Integer dice = ThreadLocalRandom.current().nextInt(1, Integer.MAX_VALUE);
+		
+		final Integer dice = ThreadLocalRandom.current().nextInt(1, Integer.MAX_VALUE);
 
-        if(dice % 3 == 0) {
-            return new Response
-                        .Builder()
-                        .request(request)
-                        .protocol(Protocol.HTTP_1_1)
-                        .code(MSClientErrorCodeTooManyRequests)
-                        .message("Too Many Requests")
-                        .addHeader(RETRY_AFTER, "10")
-                        .body(ResponseBody.create(MediaType.get("application/json"), "{\"error\": {\"code\": \"TooManyRequests\",\"innerError\": {\"code\": \"429\",\"date\": \"2020-08-18T12:51:51\",\"message\": \"Please retry after\",\"request-id\": \"94fb3b52-452a-4535-a601-69e0a90e3aa2\",\"status\": \"429\"},\"message\": \"Please retry again later.\"}}"))
-                        .build();
-        } else {
-            return chain.proceed(request);
-        }
+		if(dice % failureRate == 0) {
+			return new Response
+						.Builder()
+						.request(request)
+						.protocol(Protocol.HTTP_1_1)
+						.code(MSClientErrorCodeTooManyRequests)
+						.message("Too Many Requests")
+						.addHeader(RETRY_AFTER, retryAfterValue)
+						.body(ResponseBody.create(MediaType.get("application/json"), responseBody))
+						.build();
+		} else {
+			return chain.proceed(request);
+		}
 	}
 
 }
