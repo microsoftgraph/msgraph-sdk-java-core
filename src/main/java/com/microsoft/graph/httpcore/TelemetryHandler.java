49a107bc78922f8b05723dd18cf75bf80f4ae248
Vincent Biret <vibiret@microsoft.com>
2020-12-14 10:31:26 -0500

    - space/tabs fix to make @zengin happy :)

--- src/main/java/com/microsoft/graph/httpcore/TelemetryHandler.java
+++ src/main/java/com/microsoft/graph/httpcore/TelemetryHandler.java
@@ -11,34 +11,34 @@ import okhttp3.Response;
 
 public class TelemetryHandler implements Interceptor{
 
-	public static final String SDK_VERSION = "SdkVersion";
-	public static final String VERSION = "v1.0.6";
-	public static final String GRAPH_VERSION_PREFIX = "graph-java-core";
+    public static final String SDK_VERSION = "SdkVersion";
+    public static final String VERSION = "v1.0.6";
+    public static final String GRAPH_VERSION_PREFIX = "graph-java-core";
     public static final String JAVA_VERSION_PREFIX = "java";
     public static final String ANDROID_VERSION_PREFIX = "android";
-	public static final String CLIENT_REQUEST_ID = "client-request-id";
+    public static final String CLIENT_REQUEST_ID = "client-request-id";
 
-	@Override
-	public Response intercept(Chain chain) throws IOException {
-		final Request request = chain.request();
-		final Request.Builder telemetryAddedBuilder = request.newBuilder();
+    @Override
+    public Response intercept(Chain chain) throws IOException {
+        final Request request = chain.request();
+        final Request.Builder telemetryAddedBuilder = request.newBuilder();
 
-		TelemetryOptions telemetryOptions = request.tag(TelemetryOptions.class);
-		if(telemetryOptions == null)
-			telemetryOptions = new TelemetryOptions();
+        TelemetryOptions telemetryOptions = request.tag(TelemetryOptions.class);
+        if(telemetryOptions == null)
+            telemetryOptions = new TelemetryOptions();
 
-		final String featureUsage = "(featureUsage=" + telemetryOptions.getFeatureUsage() + ")";
-		final String javaVersion = System.getProperty("java.version");
-		final String androidVersion = getAndroidAPILevel();
-		final String sdkversion_value = GRAPH_VERSION_PREFIX + "/" + VERSION + " " + featureUsage + " " + JAVA_VERSION_PREFIX + "/" + javaVersion + " " + ANDROID_VERSION_PREFIX + "/" + androidVersion;
-		telemetryAddedBuilder.addHeader(SDK_VERSION, sdkversion_value);
+        final String featureUsage = "(featureUsage=" + telemetryOptions.getFeatureUsage() + ")";
+        final String javaVersion = System.getProperty("java.version");
+        final String androidVersion = getAndroidAPILevel();
+        final String sdkversion_value = GRAPH_VERSION_PREFIX + "/" + VERSION + " " + featureUsage + " " + JAVA_VERSION_PREFIX + "/" + javaVersion + " " + ANDROID_VERSION_PREFIX + "/" + androidVersion;
+        telemetryAddedBuilder.addHeader(SDK_VERSION, sdkversion_value);
 
-		if(request.header(CLIENT_REQUEST_ID) == null) {
-			telemetryAddedBuilder.addHeader(CLIENT_REQUEST_ID, telemetryOptions.getClientRequestId());
-		}
+        if(request.header(CLIENT_REQUEST_ID) == null) {
+            telemetryAddedBuilder.addHeader(CLIENT_REQUEST_ID, telemetryOptions.getClientRequestId());
+        }
 
-		return chain.proceed(telemetryAddedBuilder.build());
-	}
+        return chain.proceed(telemetryAddedBuilder.build());
+    }
 
     private String androidAPILevel;
     private String getAndroidAPILevel() {
--- src/test/java/com/microsoft/graph/httpcore/TelemetryHandlerTest.java
+++ src/test/java/com/microsoft/graph/httpcore/TelemetryHandlerTest.java
@@ -13,60 +13,60 @@ import okhttp3.Request;
 import okhttp3.Response;
 
 public class TelemetryHandlerTest {
-	@Test
-	public void telemetryInitTest() {
-		final TelemetryHandler telemetryHandler = new TelemetryHandler();
-		assertNotNull(telemetryHandler);
-	}
+    @Test
+    public void telemetryInitTest() {
+        final TelemetryHandler telemetryHandler = new TelemetryHandler();
+        assertNotNull(telemetryHandler);
+    }
 
-	@Test
-	public void interceptTest() throws IOException {
-		final String expectedHeader = TelemetryHandler.GRAPH_VERSION_PREFIX +"/"
-				+TelemetryHandler.VERSION;
-		final OkHttpClient client = HttpClients.createDefault(new ICoreAuthenticationProvider() {
-			@Override
-			public Request authenticateRequest(Request request) {
-				return request;
-			}
-		});
-		final Request request = new Request.Builder().url("https://graph.microsoft.com/v1.0/users/").build();
-		final Response response = client.newCall(request).execute();
-		assertNotNull(response);
-		assertTrue(response.request().header(TelemetryHandler.SDK_VERSION).contains(expectedHeader));
-		assertTrue(response.request().header(TelemetryHandler.SDK_VERSION).contains(TelemetryHandler.ANDROID_VERSION_PREFIX));
-		assertTrue(response.request().header(TelemetryHandler.SDK_VERSION).contains(TelemetryHandler.JAVA_VERSION_PREFIX));
-	}
+    @Test
+    public void interceptTest() throws IOException {
+        final String expectedHeader = TelemetryHandler.GRAPH_VERSION_PREFIX +"/"
+                +TelemetryHandler.VERSION;
+        final OkHttpClient client = HttpClients.createDefault(new ICoreAuthenticationProvider() {
+            @Override
+            public Request authenticateRequest(Request request) {
+                return request;
+            }
+        });
+        final Request request = new Request.Builder().url("https://graph.microsoft.com/v1.0/users/").build();
+        final Response response = client.newCall(request).execute();
+        assertNotNull(response);
+        assertTrue(response.request().header(TelemetryHandler.SDK_VERSION).contains(expectedHeader));
+        assertTrue(response.request().header(TelemetryHandler.SDK_VERSION).contains(TelemetryHandler.ANDROID_VERSION_PREFIX));
+        assertTrue(response.request().header(TelemetryHandler.SDK_VERSION).contains(TelemetryHandler.JAVA_VERSION_PREFIX));
+    }
 
-	@Test
-	public void arrayInterceptorsTest() throws IOException {
+    @Test
+    public void arrayInterceptorsTest() throws IOException {
 
-		final AuthenticationHandler authenticationHandler = new AuthenticationHandler(new ICoreAuthenticationProvider() {
+        final AuthenticationHandler authenticationHandler = new AuthenticationHandler(new ICoreAuthenticationProvider() {
 
-			@Override
-			public Request authenticateRequest(Request request) {
-				return request;
-			}
-		});
-		final Interceptor[] interceptors = {new RetryHandler(), new RedirectHandler(), authenticationHandler};
-		final OkHttpClient client = HttpClients.createFromInterceptors(interceptors);
-		final String expectedHeader = TelemetryHandler.GRAPH_VERSION_PREFIX +"/"
-				+TelemetryHandler.VERSION;
-		final Request request = new Request.Builder().url("https://graph.microsoft.com/v1.0/users/").build();
-		final Response response = client.newCall(request).execute();
-		assertNotNull(response);
-		assertTrue(response.request().header(TelemetryHandler.SDK_VERSION).contains(expectedHeader));
-	}
+            @Override
+            public Request authenticateRequest(Request request) {
+                return request;
+            }
+        });
+        final Interceptor[] interceptors = {new RetryHandler(), new RedirectHandler(), authenticationHandler};
+        final OkHttpClient client = HttpClients.createFromInterceptors(interceptors);
+        final String expectedHeader = TelemetryHandler.GRAPH_VERSION_PREFIX +"/"
+                +TelemetryHandler.VERSION;
+        final Request request = new Request.Builder().url("https://graph.microsoft.com/v1.0/users/").build();
+        final Response response = client.newCall(request).execute();
+        assertNotNull(response);
+        assertTrue(response.request().header(TelemetryHandler.SDK_VERSION).contains(expectedHeader));
+    }
 
-	@Test
-	public void arrayInterceptorEmptyTest() throws IOException {
-		final Interceptor[] interceptors = null;
-		final OkHttpClient client = HttpClients.createFromInterceptors(interceptors);
-		final String expectedHeader = TelemetryHandler.GRAPH_VERSION_PREFIX +"/"
-				+TelemetryHandler.VERSION;
-		final Request request = new Request.Builder().url("https://graph.microsoft.com/v1.0/users/").build();
-		final Response response = client.newCall(request).execute();
-		assertNotNull(response);
-		assertTrue(response.request().header(TelemetryHandler.SDK_VERSION).contains(expectedHeader));
-	}
+    @Test
+    public void arrayInterceptorEmptyTest() throws IOException {
+        final Interceptor[] interceptors = null;
+        final OkHttpClient client = HttpClients.createFromInterceptors(interceptors);
+        final String expectedHeader = TelemetryHandler.GRAPH_VERSION_PREFIX +"/"
+                +TelemetryHandler.VERSION;
+        final Request request = new Request.Builder().url("https://graph.microsoft.com/v1.0/users/").build();
+        final Response response = client.newCall(request).execute();
+        assertNotNull(response);
+        assertTrue(response.request().header(TelemetryHandler.SDK_VERSION).contains(expectedHeader));
+    }
 
 }
