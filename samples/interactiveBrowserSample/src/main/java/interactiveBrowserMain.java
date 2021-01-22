add6834eeaeef56cd72f919cba69d0e5cd7cbe07
Vincent Biret <vibiret@microsoft.com>
2021-01-22 08:40:56 -0500

    - replaces ID by placeholder

--- samples/interactiveBrowserSample/src/main/java/interactiveBrowserMain.java
+++ samples/interactiveBrowserSample/src/main/java/interactiveBrowserMain.java
@@ -13,7 +13,7 @@ public class interactiveBrowserMain {
     //Replace CLIENT_ID with your own client id from an adequately configured app
     //for requirements visit:
     //https://github.com/Azure/azure-sdk-for-java/wiki/Set-up-Your-Environment-for-Authentication#enable-applications-for-interactive-browser-oauth-2-flow
-    private final static String CLIENT_ID = "199e4de3-dd3b-4a51-b78a-86b801246e20";
+    private final static String CLIENT_ID = "<your client/app id>";
 
     //Set the scopes for your ms-graph request
     private final static List<String> SCOPES = Arrays.asList("User.ReadBasic.All");
