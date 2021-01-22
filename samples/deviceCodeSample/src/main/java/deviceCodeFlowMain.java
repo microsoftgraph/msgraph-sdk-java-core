d7d669a3d2f7c3c9fd0ce4031dc12d27ecfb334f
Vincent Biret <vibiret@microsoft.com>
2021-01-22 08:27:10 -0500

    Apply suggestions from code review

--- samples/deviceCodeSample/src/main/java/deviceCodeFlowMain.java
+++ samples/deviceCodeSample/src/main/java/deviceCodeFlowMain.java
@@ -12,10 +12,10 @@ import java.util.List;
 
 public class deviceCodeFlowMain {
 
-    //Replace CLIENT_ID with your own client id from an adequately configured app
+    //Replace CLIENT_ID with your own client id from an app that is configured according to the requirements below
     //for requirements visit:
     //https://github.com/Azure/azure-sdk-for-java/wiki/Set-up-Your-Environment-for-Authentication#enable-applications-for-device-code-flow
-    private final static String CLIENT_ID = "cfb5d84c-c88f-466c-81fb-0fe9fa8da052";
+    private final static String CLIENT_ID = "<your client/app id>";
 
     //Set the scopes for your ms-graph request
     private final static List<String> SCOPES = Arrays.asList("User.ReadBasic.All", "User.Read");
