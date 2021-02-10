480b52178878a854cd48adac2fef75cd2fa38b21
Vincent Biret <vibiret@microsoft.com>
2021-02-10 13:26:15 -0500

    - avoid instantiating a builder when it's not needed

--- src/main/java/com/microsoft/graph/http/BaseCollectionRequest.java
+++ src/main/java/com/microsoft/graph/http/BaseCollectionRequest.java
@@ -120,10 +120,10 @@ public abstract class BaseCollectionRequest<T, T2 extends ICollectionResponse<T>
     public T3 buildFromResponse(@Nonnull final T2 response) {
         final List<com.microsoft.graph.options.Option> options = new java.util.ArrayList<com.microsoft.graph.options.Option>();
         try {
-            final Object builder = this.collRequestBuilderClass
+            final Object builder = response.nextLink() == null ? null : this.collRequestBuilderClass
                     .getConstructor(String.class, IBaseClient.class, java.util.List.class)
                     .newInstance(response.nextLink(), getBaseRequest().getClient(), options);
-            final T3 page = (T3)this.collectionPageClass.getConstructor(response.getClass(), builder.getClass()).newInstance(response, response.nextLink() == null ? null : builder);
+            final T3 page = (T3)this.collectionPageClass.getConstructor(response.getClass(), this.collRequestBuilderClass).newInstance(response, builder);
             return page;
         } catch(IllegalArgumentException | InstantiationException | IllegalAccessException | InvocationTargetException | NoSuchMethodException | SecurityException ex) {
             throw new ClientException("Could not find the required class", ex);
