a017c5d815e08fda7f5a0160e7b23d53f2f50329
Vincent Biret <vibiret@microsoft.com>
2021-02-15 10:03:37 -0500

    - fixes mocks after linting

--- src/test/java/com/microsoft/graph/httpcore/TelemetryHandlerTest.java
+++ src/test/java/com/microsoft/graph/httpcore/TelemetryHandlerTest.java
@@ -2,9 +2,13 @@ package com.microsoft.graph.httpcore;
 
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 import java.io.IOException;
+import java.net.URL;
+import java.util.concurrent.CompletableFuture;
 
 import com.microsoft.graph.authentication.IAuthenticationProvider;
 import org.junit.jupiter.api.Test;
@@ -22,11 +26,12 @@ public class TelemetryHandlerTest {
     }
 
     @Test
-    @SuppressWarnings("unchecked")
     public void interceptTest() throws IOException {
         final String expectedHeader = TelemetryHandler.GRAPH_VERSION_PREFIX +"/"
                 +TelemetryHandler.VERSION;
-        final OkHttpClient client = HttpClients.createDefault(mock(IAuthenticationProvider.class));
+        final IAuthenticationProvider authProvider = mock(IAuthenticationProvider.class);
+        when(authProvider.getAuthorizationTokenAsync(any(URL.class))).thenReturn(CompletableFuture.completedFuture(""));
+        final OkHttpClient client = HttpClients.createDefault(authProvider);
         final Request request = new Request.Builder().url("https://graph.microsoft.com/v1.0/users/").build();
         final Response response = client.newCall(request).execute();
         assertNotNull(response);
@@ -36,10 +41,10 @@ public class TelemetryHandlerTest {
     }
 
     @Test
-    @SuppressWarnings("unchecked")
     public void arrayInterceptorsTest() throws IOException {
-
-        final AuthenticationHandler authenticationHandler = new AuthenticationHandler(mock(IAuthenticationProvider.class));
+        final IAuthenticationProvider authProvider = mock(IAuthenticationProvider.class);
+        when(authProvider.getAuthorizationTokenAsync(any(URL.class))).thenReturn(CompletableFuture.completedFuture(""));
+        final AuthenticationHandler authenticationHandler = new AuthenticationHandler(authProvider);
         final Interceptor[] interceptors = {new RetryHandler(), new RedirectHandler(), authenticationHandler};
         final OkHttpClient client = HttpClients.createFromInterceptors(interceptors);
         final String expectedHeader = TelemetryHandler.GRAPH_VERSION_PREFIX +"/"
