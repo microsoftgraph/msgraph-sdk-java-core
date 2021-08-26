c1a12b1c871420ff278889510f32f06a6021b65f
ramsessanchez <63934382+ramsessanchez@users.noreply.github.com>
2021-08-26 10:26:36 -0700

    Update BaseAuthenticationProvider.java

--- src/main/java/com/microsoft/graph/authentication/BaseAuthenticationProvider.java
+++ src/main/java/com/microsoft/graph/authentication/BaseAuthenticationProvider.java
@@ -6,7 +6,7 @@ import java.util.HashSet;
 import java.util.Locale;
 
 import javax.annotation.Nonnull;
-//CI TEST2, IGNORE
+//CI TEST3, IGNORE
 /**
  * Provides basic common methods for all authentication providers
  */
