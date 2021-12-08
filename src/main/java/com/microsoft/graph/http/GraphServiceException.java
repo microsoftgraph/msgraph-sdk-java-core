4860c0aa4faf2ecdc7a4f7dafa10c8ff6d97e531
ramsessanchez <63934382+ramsessanchez@users.noreply.github.com>
2021-12-08 02:07:24 -0800

    Partial GrapServiceException suppress

--- src/main/java/com/microsoft/graph/http/GraphServiceException.java
+++ src/main/java/com/microsoft/graph/http/GraphServiceException.java
@@ -203,7 +203,7 @@ public class GraphServiceException extends ClientException {
      */
     @Nonnull
     public List<String> getResponseHeaders() {
-        return responseHeaders;
+        return Collections.unmodifiableList(responseHeaders);
     }
 
     /**
@@ -212,7 +212,7 @@ public class GraphServiceException extends ClientException {
      */
     @Nullable
     public GraphErrorResponse getError() {
-        return error;
+        return error; //Copy
     }
 
     /**
@@ -239,7 +239,7 @@ public class GraphServiceException extends ClientException {
      */
     @Nonnull
     public List<String> getRequestHeaders() {
-        return requestHeaders;
+        return Collections.unmodifiableList(requestHeaders);
     }
 
     /**
