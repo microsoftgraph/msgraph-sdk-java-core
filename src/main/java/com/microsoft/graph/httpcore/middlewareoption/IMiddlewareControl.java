9528617145b8955769172ce851d487d18a09417d
ramsessanchez <ramses.sanchez99@gmail.com>
2022-08-09 18:32:12 -0700

    Delete all files which will need to be changes/not needed for kiota core

--- src/main/java/com/microsoft/graph/httpcore/middlewareoption/IMiddlewareControl.java
+++
@@ -1,8 +0,0 @@
-package com.microsoft.graph.httpcore.middlewareoption;
-
-/**
- * Option to be passed to one of the middlewares provided by this SDK
- */
-public interface IMiddlewareControl {
-
-}
--- src/test/java/com/microsoft/graph/serializer/CollectionResponseOfString.java
+++
@@ -1,6 +0,0 @@
-package com.microsoft.graph.serializer;
-
-import com.microsoft.graph.http.BaseCollectionResponse;
-
-public class CollectionResponseOfString extends BaseCollectionResponse<String> {
-}
