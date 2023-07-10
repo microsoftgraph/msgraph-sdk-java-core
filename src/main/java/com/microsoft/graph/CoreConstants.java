a33de00f993944be957aed8b821d10cf42344e46
ramsessanchez <63934382+ramsessanchez@users.noreply.github.com>
2023-07-10 15:13:05 -0700

    bumps to 3.0.6

--- src/main/java/com/microsoft/graph/CoreConstants.java
+++ src/main/java/com/microsoft/graph/CoreConstants.java
@@ -11,7 +11,7 @@ public final class CoreConstants {
     private static class VersionValues {
         private static final int MAJOR = 3;
         private static final int MINOR = 0;
-        private static final int PATCH = 5;
+        private static final int PATCH = 6;
     }
 
     /**
