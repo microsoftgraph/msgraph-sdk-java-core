16ce583c64045517d23b15faf9479e32a86ad110
ramsessanchez <63934382+ramsessanchez@users.noreply.github.com>
2024-02-09 14:49:06 -0800

    formating

--- src/main/java/com/microsoft/graph/core/content/BatchRequestContent.java
+++ src/main/java/com/microsoft/graph/core/content/BatchRequestContent.java
@@ -212,13 +212,13 @@ public class BatchRequestContent {
             //Remove the header if it is some version of 'authorization'
             //RemoveAll utilizes ignoreCase natively
             headers = headers.newBuilder().removeAll(AUTHORIZATION_HEADER_KEY).build();
-            if((headers.size() != 0 || requestBody != null)) {
-                    writer.name(CoreConstants.BatchRequest.HEADERS);
-                    writer.beginObject();
-                    for (int i = 0; i < headers.size(); i++) {
-                        writer.name(headers.name(i)).value(headers.value(i));
-                    }
-                    writer.endObject();
+            if(headers.size() != 0 || requestBody != null) {
+                writer.name(CoreConstants.BatchRequest.HEADERS);
+                writer.beginObject();
+                for (int i = 0; i < headers.size(); i++) {
+                    writer.name(headers.name(i)).value(headers.value(i));
+                }
+                writer.endObject();
             }
             writer.endObject();
     }
