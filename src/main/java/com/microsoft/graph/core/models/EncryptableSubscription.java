53f4ef54296c7c919dbe57d9b108f8360f39c443
Philip Gichuhi <pgichuhi@microsoft.com>
2025-02-05 10:01:25 +0300

    add EncryptableSubscription

---
+++ src/main/java/com/microsoft/graph/core/models/EncryptableSubscription.java
@@ -0,0 +1,41 @@
+package com.microsoft.graph.core.models;
+
+import java.security.cert.CertificateEncodingException;
+import java.security.cert.X509Certificate;
+import java.util.Base64;
+import java.util.Objects;
+
+import jakarta.annotation.Nonnull;
+
+/**
+ * EncryptableSubscription interface
+ */
+public interface EncryptableSubscription {
+
+    /**
+     * Sets the encryption certificate
+     * @param certificate Base-64 encoded certificate to be used by Microsoft Graph to encrypt resource data
+     */
+    public void setEncryptionCertificate(String certificate);
+
+    /**
+     * Returns the encryption certificate
+     * @return encryption certificate
+     */
+    public String getEncryptionCertificate();
+
+    /**
+     * Converts an X.509 Certificate object to Base-64 string and adds to the encryptableSubscription provided
+     * @param subscription encryptable subscription
+     * @param certificate X.509 Certificate
+     * @throws CertificateEncodingException
+     */
+    public static void addPublicEncryptionCertificate(@Nonnull final EncryptableSubscription subscription, @Nonnull final X509Certificate certificate) throws CertificateEncodingException {
+        Objects.requireNonNull(subscription);
+        Objects.requireNonNull(certificate);
+        subscription.setEncryptionCertificate(
+            Base64.getEncoder().encodeToString(certificate.getEncoded())
+        );
+    }
+
+}
